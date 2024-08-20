9879dd0778ce0161f4cf454eb806d9c0e6d02815
agent19music <seanmotanya@gmail.com>
2024-08-20 10:27:36 +0300

    Add info into object

---
+++ src/app/data/resume.tsx
@@ -0,0 +1,195 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Dillion Verma",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "Nairobi, KE",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Postgres",
+    "Docker",
+   
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "seanmotanya@gmail.com",
+    tel: "+254 745 071 299",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/agent19music",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/sean-motanya/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/ufwsean",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Vitapharm Health",
+      href: "https://vitapharmcosmetics.co.ke",
+      badges: [],
+      location: "Hybrid",
+      title: "Frontend Software Engineer",
+      logoUrl: "/vitapharmlogo.png",
+      start: "April 2024",
+      end: "date",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "E-Kommerce Ltd",
+      href: "https://mitremedia.com/",
+      badges: [],
+      location: "Toronto, ON",
+      title: "Full Stack Software Engineer",
+      logoUrl: "/mitremedia.png",
+      start: "June 2024",
+      end: "date",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "Moringa School",
+      href: "https://moringaschool.com",
+      degree: "Certificate in Software Engineering",
+      logoUrl: "/moringalogo.jpeg",
+      start: "2023",
+      end: "2024",
+    },
+    {
+      school: "Riara University",
+      href: "https://riarauniversity.ac.ke",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/rulogo.jpg",
+      start: "2021",
+      end: "2025",
+    }
+  ],
+  projects: [
+    {
+      title: "Campo Social",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "React.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    }
+  ],
+  hackathons: [
+    {
+      title: "A2SV 2024",
+      dates: "November 23rd - 25th, 2018",
+      location: "London, Ontario",
+      description:
+        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+    {
+      title: "Hack The North",
+      dates: "September 14th - 16th, 2018",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed a mobile application which delivers university campus wide events in real time to all students.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    }
+
+  ],
+} as const;
