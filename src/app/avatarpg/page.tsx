817f60d61caf7cf8cbd14d30eb30a1d03b924b88
agent19music <seanmotanya@gmail.com>
2024-08-22 11:54:40 +0300

    Add tsx page to render avatar

---
+++ src/app/avatarpg/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SeanAvatar from '@/components/avatar'
+
+export default function page() {
+  return (
+    <div className='h-100 w-100'>
+        <SeanAvatar />
+    </div>
+  )
+}
