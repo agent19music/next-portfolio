e2d4ddb66a01d9da0b729b3659265ff8405e2158
agent19music <seanmotanya@gmail.com>
2025-07-22 14:44:22 +0300

    add in open peeps avatar (˵ ¬ᴗ¬˵)

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -60,7 +60,9 @@ export async function GET(request: NextRequest) {
 <img
   src="https://pub-c6a134c8e1fd4881a475bf80bc0717ba.r2.dev/Open%20Peeps%20-%20Avatar-smile.png"
   alt="Sean Motanya"
-  style={{ borderRadius: "60px" }}
+  style={{ borderRadius: "60px", marginBottom: '40px' }}
+  width={342px}
+  height={400px}
 />            </div>
             <h1
               style={{
