d0563cb113ef114ae3826af98bde5e2faee7e1da
agent19music <seanmotanya@gmail.com>
2025-07-22 14:16:40 +0300

    update open peeps avatar (˵ ¬ᴗ¬˵)

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import Image from 'next/image';
 
 export const runtime = 'edge';
 
@@ -66,8 +65,13 @@ export async function GET(request: NextRequest) {
                 color: offWhite,
               }}
             >
-        <Image src="https://pub-c6a134c8e1fd4881a475bf80bc0717ba.r2.dev/Open%20Peeps%20-%20Avatar-smile.png" alt="Sean Motanya" width={120} height={120} />              
-            </div>
+<img
+  src="https://pub-c6a134c8e1fd4881a475bf80bc0717ba.r2.dev/Open%20Peeps%20-%20Avatar-smile.png"
+  alt="Sean Motanya"
+  width={120}
+  height={120}
+  style={{ borderRadius: "60px" }}
+/>            </div>
             <h1
               style={{
                 fontSize: '64px',
